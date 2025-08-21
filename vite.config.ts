a5f3a8c2703f653e2c74598037d8c6a523d5ba53
boris.mil.96 <boris.mil.96@gmail.com>
2025-08-21 20:08:24 +0300

    Vercel deployment 0.6

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.ts
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
 
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   base: process.env.VITE_BASE_URL || 'react-vite-deploy',
