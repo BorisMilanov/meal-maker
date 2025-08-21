4276af58156bad1cade29f1f51ceeec4ddfcec09
boris.mil.96 <boris.mil.96@gmail.com>
2025-08-21 23:07:27 +0300

    Vercel deployment 0.8

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: process.env.VITE_BASE_URL || 'react-vite-deploy',
+  base: process.env.VITE_BASE_URL || '/meal-maker',
   server: {
     proxy: {
 
