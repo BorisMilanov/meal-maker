c8fb435cc26ee7584308b35827bd1a7a87134e03
boris.mil.96 <boris.mil.96@gmail.com>
2025-08-21 15:48:03 +0300

    Vercel deployment 0.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import InfoMealMobile from "./services/InfoMealMobile.tsx";
-import { BrowserRouter as  Router ,Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as  Router , Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -8,7 +8,7 @@ import "./index.css"
 export default function App() {
     return (
         <Router>
-            {/* Navigation */}
+             Navigation
             <nav className="p-4 bg-orange-500 text-white flex gap-4">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
@@ -20,6 +20,7 @@ export default function App() {
             {/* Page Routes */}
             <div className="p-4">
                 <Routes>
+
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
